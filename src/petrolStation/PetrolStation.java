e40c46ef286c4737d70118e8160597973ae6870f
kkgm13 <kkgm13013@gmail.com>
2017-03-07 14:49:26 +0000

    Javadoc Author and Version added

--- src/petrolStation/PetrolStation.java
+++ src/petrolStation/PetrolStation.java
@@ -1,5 +1,16 @@
 package petrolStation;
 
+/**
+ * 
+ * @author Kelvin M.
+ * @author Tristan P
+ * @author Matas B.
+ * @author Milton R.
+ * @author Jordan L.
+ * @version 0.1
+ *
+ */
+
 public class PetrolStation {
 	private int maxQueue;
 }
