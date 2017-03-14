e76e92453d62e145faec55d403e67f321214ffbe
Tristan <trisperk@hotmail.com>
2017-03-14 14:49:46 +0000

    completed vehicle queue (i think)

--- src/aston/resources/VehicleQueue.java
+++ src/aston/resources/VehicleQueue.java
@@ -7,15 +7,54 @@ public class VehicleQueue {
 	
 	private Queue<Vehicle> q;
 	private double queueSpace = Config.queueSize;
+	private double spaceTaken = 0;
 	
+	//Constructor
 	public VehicleQueue()
 	{
 		q = new PriorityQueue<Vehicle>();
 	}
 	
-	public void addVehicle(Vehicle v)
+	/*
+	 * Adds a vehicle to the queue if there is enough space for it to fit, adds the size of the vehicle to the space taken
+	 * 
+	 * @return boolean true if vehicle is added, otherwise false
+	 */
+	public boolean addVehicle(Vehicle v)
 	{
-		if (queueSpace < v.space)
+		if (queueSpace < v.getVehicleSize() + spaceTaken)
+		{
+			spaceTaken += v.getVehicleSize();
+			q.add(v);
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
-
+	
+	/*
+	 * Gets the vehicle at the front of the queue
+	 * 
+	 * @return Vehicle The vehicle at the front
+	 */
+	public Vehicle getFrontVehicle()
+	{
+		Vehicle v = q.peek();
+		return v;
+	}
+	
+	/*
+	 * Removes a vehicle from the front of the queue and changes the ammount of space taken in the queue
+	 */
+	public void removeFrontVehicle()
+	{
+		Vehicle v = q.peek();
+		if (v != null)
+		{
+			q.remove();
+			spaceTaken -= v.getVehicleSize();
+		}
+	}
 }
