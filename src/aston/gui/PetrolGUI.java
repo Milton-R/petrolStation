4a20af42e03031bda8b56787f60218eb6a0e3825
kkgm13 <kkgm013@gmail.com>
2017-03-20 14:46:28 +0000

    Added standard steps and Step one code

--- src/aston/gui/PetrolGUI.java
+++ src/aston/gui/PetrolGUI.java
@@ -11,20 +11,35 @@ public class PetrolGUI {
 	//JFrame for the 
 	private JFrame mainFrame;
 	private Object o;
-	
+	private JTextArea log;
 	
 	
 	public PetrolGUI(Object o){
 		this.o = o;
 		
 		// Step 1: Create the components
+			//Title
+		JLabel title = new JLabel();
+			//Simulation
+		JPanel simPanel = new JPanel();
+			//Log Section
+		JPanel logPanel = new JPanel();
+		JScrollPane listScroll = new JScrollPane(logPanel);
+		
+		JButton quitButton = new JButton();
+		JButton newButton = new JButton();
+		
 		// Step 2: Set the properties of the components 
+		title.setText("Petrol Station Simulation");
 		// Step 3: Create containers to hold the components
 		mainFrame = new JFrame("Petrol Simulator");
 		// Step 4: Specify LayoutManagers
 		// Step 5: Add components to containers 
 		// Step 6: Arrange to handle events in the user interface
+		
 		// Step 7: Display the GUI
+		mainFrame.pack();
+		mainFrame.setVisible(true);
 	}
 	
 }
