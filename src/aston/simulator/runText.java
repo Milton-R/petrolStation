293a918f30b67a586c763738ad3ea63567d3a18a
Tristan <trisperk@hotmail.com>
2017-03-27 16:20:48 +0100

    loads of shit

---
+++ src/aston/simulator/runText.java
@@ -0,0 +1,21 @@
+package aston.simulator;
+
+public class runText {
+	
+	/**
+	 * Create a simulation and run it for a specified number of steps, based on lab5 simulator
+	 */
+	public static void main(String[] args) {
+		int numSteps = 1;  // By default, run for 1 step
+		if (args.length >= 1) {
+			numSteps = Integer.parseInt(args[0]);
+		}
+		if (numSteps <= 0) 
+			numSteps = 1;
+		
+		Simulator s = new Simulator();
+		s.simulate(numSteps);
+		s.updateText();
+	}
+
+}
