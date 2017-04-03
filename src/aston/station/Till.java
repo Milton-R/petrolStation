176999dbc7afec8d5a675a8c8c0dfaf20ae10e4c
Tristan <trisperk@hotmail.com>
2017-04-03 22:50:47 +0100

    worked on Customer class

--- src/aston/station/Till.java
+++ src/aston/station/Till.java
@@ -1,5 +1,20 @@
 package aston.station;
 
+import aston.vehicles.*;
+import java.util.ArrayList;
+
 public class Till {
+	
+	private ArrayList<Customer> shoppingCustomers = new ArrayList<Customer>();
+	
+	public void addCustomer(Customer c)
+	{
+		shoppingCustomers.add(c);
+	}
+	
+	public void removerCustomer(Customer c)
+	{
+		shoppingCustomers.remove(c);
+	}
 
 }
