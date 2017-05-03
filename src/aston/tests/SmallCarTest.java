15d5e4042cc10f4928aed3e29a2fbaa89bbf0a32
Milton-R <ramalhom@aston.ac.uk>
2017-05-03 20:53:36 +0100

    testing classes

--- src/aston/tests/SmallCarTest.java
+++ src/aston/tests/SmallCarTest.java
@@ -3,33 +3,54 @@ import aston.station.PetrolStation;
 import aston.vehicles.SmallCar;
 import aston.vehicles.Vehicle;
 import aston.station.PetrolStation;
-import org.junit.BeforeClass;
 import aston.resources.*;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
 public class SmallCarTest {
 	
+	String n = "3";
+	int steps= 30;
+	PetrolStation ps = new PetrolStation(new Config());
+	SmallCar testcar = new SmallCar(n, ps, steps);
+	
+	
 	@Test 
-	public void testgetTankSize()
+	public void testGetTankSize()
 	{
-		String n = "3";
-		int steps= 30;
-		PetrolStation ps = new PetrolStation(new Config());
-		SmallCar testcar = new SmallCar(n, ps, steps);
 		assertEquals("cba to type error message", 7 ,testcar.getTankSize(), 2);
-		
-		assertTrue("small car's size must be 1",!( 1.0 == testcar.getVehicleSize()));
-		
-		assertEquals("small car's shop time limit bust be 30", 30, ((Vehicle) testcar).getShopTimeLimit());
-		//assertEquals("small car's shopping probability must be 0.3 ", 0.3, ((Vehicle) testcar).getShoppingProbability());
-		assertEquals("small car's time taken must be between 12 and 24",  12, ((Vehicle) testcar).getTimeTakenShopping(), 12 );
-		assertEquals("small car's money spent must be betweeen 500 to 1000 pence", 500, ((Vehicle) testcar).getMoneySpentShopping(), 500 );
+
 	}
 	
-	//make it loop , and keep asing if it's done filling up
-	//when it's false , it is done
-	//store the number of steps taken to fill it up and make sure it lies within the tanksize range
+	@Test 
+	public void testGetVehicleSize()
+	{
+		assertTrue("small car's size must be 1",( 1.0 == testcar.getVehicleSize()));
+		
+	}
 	
+	
+	//make it loop , and keep using if it's done filling up
+	//when it's false , it is done
+	//store the number of steps taken to fill it up and make sure it lies within the tank size range
+		
+
+  @Test 
+   public void testGetShoppingtime()
+   {
+	  assertEquals("small car's shop time limit bust be 30", 30 ,testcar.getShopTimeLimit(), 12);
+   }
+  
+  @Test 
+  public void testGetShoppingProbability()
+  {
+	  assertTrue("small car's shopping probability must be 0.3 ", (0.3==testcar.getShoppingProbability()));
+  }
+  
+  @Test 
+  public void testGetMoneySpentShopping()
+  {
+	  assertEquals("small car's money spent must be betweeen 500 to 1000 pence", 500, ((Vehicle) testcar).getMoneySpentShopping(), 500 );
+  }
 	
 }
